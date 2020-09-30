b625cef576f081cd976ccca52ecbc8ce551e91dc
terranadams <terran.adams7225@stu.mtec.edu>
2020-09-29 19:02:30 -0600

    created delete feature

--- todo.js
+++ todo.js
@@ -9,6 +9,7 @@ let newItem = document.getElementById("newItem")
 let items = document.getElementById("items")
 
 form.addEventListener("submit", (e) => {
+
     e.preventDefault()
 
     createList(field.value)
@@ -16,6 +17,7 @@ form.addEventListener("submit", (e) => {
 })
 
 newItem.addEventListener("submit", (e) => {
+
     e.preventDefault()
 
     createItem(newItem.value)
@@ -27,21 +29,22 @@ newItem.addEventListener("submit", (e) => {
 function createList(x) {
     let label = `<div class="text-left col-xs-3"><h3>${x}</h3><br>
     <input id="newItem" type="text" autocomplete="off">
-    <button onclick="createItem(this)" class="btn btn-success border border-dark">Add Item</button>
+    <button onclick="createItem(newItem.value)" class="btn btn-success border border-dark">Add Item</button>
     <button onclick="editList(this)" class="btn btn-warning border border-dark">Edit</button>
-    <button onclick="deleteList(this)" class="btn btn-danger border border-dark">Delete</button></div>
+    <button onclick="deleteList(this)" class="btn btn-danger border border-dark">Delete</button>
     <ul id="items">
-    </ul>`
+    </ul>
+    </div>`
 list.insertAdjacentHTML("beforeend", label);
 field.value = "";
 field.focus();
 }
 
 function createItem(x) {
-    let listItem = document.createElement('li')
+    let newListItemHTML = `<li>${x}</li>`
+    items.insertAdjacentHTML("beforeend", newListItemHTML)
+}
 
-    listItem.textContent = x 
-
-    items.appendChild(listItem)
-
+function deleteList(thisElement) {
+    thisElement.parentElement.remove()
 }
