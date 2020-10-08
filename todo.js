6b767f294aca79fb4fd284ae7d9c879110846b4e
terranadams <terran.adams7225@stu.mtec.edu>
2020-10-08 09:25:51 -0600

    started edit function and moved buttons in heading

--- todo.js
+++ todo.js
@@ -24,7 +24,7 @@ function createList(listName) {
         <input id="newItem" type="text" autocomplete="off">
 
         <button onclick="createItem(document.getElementById('${listName}').getElementsByTagName('input')[0].value, '${listName}')" class="btn btn-success border border-dark">Add Item</button>
-        <button onclick="editList(this)" class="btn btn-warning border border-dark">Edit</button>
+        <button onclick="editList(${listName})" class="btn btn-warning border border-dark">Edit</button>
         <button onclick="deleteList(this)" class="btn btn-danger border border-dark">Delete</button>
 
         <ul id="items">
@@ -62,13 +62,15 @@ function printList(listName) {
 
 
 function editList(listName) {
-    
+    let newValue = prompt("What is the new list name for this list?")
+    listDictionary[listName] = newValue
+    document.getElementById('listName').getElementsByTagName('h3')[0].innerHTML = newValue;
 }
 
 function deleteList(thisElement) {
     thisElement.parentElement.remove()
 }
 
-function deleteCompleted() {
+function clearCompleted() {
 
 }
